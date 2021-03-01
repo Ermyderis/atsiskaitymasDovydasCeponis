c6fef653397c16bc002cb0c6d81ee8a6a1a7aa8c
dovyd <dovydas0000@gmail.com>
2021-03-01 12:22:54 +0200

    pirmas

--- main.py
+++ main.py
@@ -1,16 +1,58 @@
-# This is a sample Python script.
+"""
+Sukurti funkciją, kuri priima
+tris parametrus: n - tekstas, a - tekstas, kurio
+simboliai yra teigiami, b - tekstas, kuriosimboliai
+yra neigiami. Teigiami simboliai yra verti 1, neigiami
+verti -1, o simboliai, kurių nėra nei a, nei b teksteyra vertas 0.
+Funkcija suskaičiuoja teksto įvertį ir jį grąžina
+"""
+#Fukcija priimanti 3 kintamuosius
+def pirmas(n, a, b):
+    try:
+        #Tekstasn gauna n kintamojo reiksme
+        tekstasn = n
+        #teigiamia gauna a kintamojo reiksme
+        teigiamia = a
+        #neigiamib gauna b kintamojo reiksme
+        neigiamib = b
+        #priskiriamos 0 reiksmes
+        pirmas = 0
+        antras = 0
+        #for ciklas skirtas pakartotinai eiti per tekstasn
+        for i in tekstasn:
+            #ieskoti teigiamiems nariams
+            for y in teigiamia:
+                #Jei teigiamame pridedama po 1
+                if(i == y):
+                    pirmas = pirmas + 1
+            #ieskoti neigiamiems nariam
+            for z in neigiamib:
+            #jei neigiamam atimama po 1
+                if(i==z):
+                    antras = antras + 1
+        print(pirmas-antras)
+    except:
+        print("Klaida, naudokite tik teksta")
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+print("Pagal pvz 1: ")
+pirmas("keturiolika", "ktur", "ila")
+
+print("Pagal pvz 2: ")
+pirmas("vienas du trys", "vn ", "ayds")
+
+print("Pvz su klaida: ")
+pirmas("keturiolika", "ktur", 1)
+
+"""
+Sukurti funkciją, kuri priima vieną
+parametrą a - vienos dimensijos sąrašas.
+Funkcijoje atliekamas sąrašofiltravimas paliekant
+reikšmes iš intervalo 10-100. Funkcija grąžina išfiltruotų
+reikšmių: vidurkį, didžiausią irmažiausią reikšmes, bei sumą.
+Sukurti antrą funkciją, kuri priima vieną parametrą b - sąrašą
+sudarytą iš sąrašųir kiekvienam sąrašo elementui
+iškviečia pirmą funkciją ir atspausdina gautą rezultatą
+"""
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
